a61e1dfe5627e5d55729960c9658111f05214989
PrakharMishra31 <prakharmishra3197@gmail.com>
2022-01-06 21:11:52 +0530

    Pricing UI using React

---
+++ src/Components/FeaturesList/FeatureList.js
@@ -0,0 +1,47 @@
+import "./FeatureList.css";
+
+function FeatureList(props) {
+  const features = [
+    "Single User",
+    "5GB Storage",
+    "Unlimited Public Projects",
+    "Community Access",
+    "Unlimited Private Projects",
+    "Dedicated Phone Support",
+    "Free Subdomain",
+    "Monthly Status Reports",
+  ];
+
+  return (
+    <div>
+      <ul>
+        {features.map((el, i) => {
+          if (props.features.includes(el)) {
+            return (
+              <div key={i} className="list-component">
+                <i className="fa fa-check"></i>
+                <li className="feature-list">
+                  {props.features.length === features.length &&
+                  el === "Free Subdomain"
+                    ? "Unlimited Free Subdomains"
+                    : el}
+                </li>
+              </div>
+            );
+          } else {
+            return (
+              <div key={i} className="list-component">
+                <i className="fa fa-times"></i>
+                <li className="feature-list disable" id={i}>
+                  {el}
+                </li>
+              </div>
+            );
+          }
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default FeatureList;
